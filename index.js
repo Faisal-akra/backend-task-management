fb049374f750b2e79923f2585bd09530dcda0192
ADMIN <zjan45097@gmail.com>
2025-07-06 16:36:11 +0500

    normalize line endings

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+
+
+const port = "https://localhost9000";
+
+app.listen(port, () => {
+  console.log(`port is running on ${port}`);
+});
