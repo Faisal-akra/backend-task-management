7eaeaaf4eb1bfeb7958303b3b721265210eee305
ADMIN <zjan45097@gmail.com>
2025-07-07 22:42:47 +0500

    task fetch api is created

--- controllers/task.js
+++ controllers/task.js
@@ -1,4 +1,5 @@
 const taskModel = require("../models/task")
+const errorMsg = "unexpected error";
 
 const createTask = async (req, res) => {
   try {
@@ -24,11 +25,37 @@ const createTask = async (req, res) => {
       task: task,
     });
   } catch (error) {
-    console.log(error, "error");
+    console.log(error, errorMsg);
     res.status(404).json({
       msg: "error",
     });
   }
 };
 
-module.exports = createTask;
+
+
+const fetchAllTask = async(req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const task = await taskModel.find({user: userId});
+
+    if(!task) {
+      return res.status(404).json({
+        msg: "this is task is nothing"
+      })
+    }
+
+    res.status(200).json({
+      msg: "task fetch successfully",
+      task: task
+    })
+  } catch (error) {
+    console.log(error, errorMsg)
+  }
+}
+
+
+
+
+module.exports = {createTask, fetchAllTask};
--- routes/task.js
+++ routes/task.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 
 const verifyUSer = require("../middleware/middleware");
-const createTask = require("../controllers/task");
+const {createTask, fetchAllTask} = require("../controllers/task");
 const taskRoute = express.Router();
 
 taskRoute.post("/createTask", verifyUSer , createTask);
+taskRoute.get('/fetchTask', verifyUSer, fetchAllTask)
 
 
 module.exports = taskRoute;
